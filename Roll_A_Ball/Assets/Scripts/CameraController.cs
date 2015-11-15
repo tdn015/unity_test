8e1037396807d455d66b8eb4e4fc116dc645b810
Trent Nguyen <nguyen.trent15@gmail.com>
2015-11-14 18:46:12 -0800

    roll_a_ball unity project

---
+++ Roll_A_Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset; // position of camera minus position of player
+
+	// Use this for initialization
+	void Start () {
+
+		// distance between player and camera
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame; guaranteed to run after all items are processed, not just position
+	void LateUpdate () {
+
+		// when player moves, camera stays (offset distance) away
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll_A_Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; // shows up in inspector as a property
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb; // create RigidBody object
+	private int count;
+
+	// Initializes when game starts
+	void Start ()
+	{
+		// Set RigidBody object to RigidBody component on Unity
+		rb = GetComponent<Rigidbody> (); 
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	// physics update
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// create vector3 object and map it to (x, y, z) positions
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if(count >= 12){
+			winText.text = "You win!";
+		}
+	}
+}
